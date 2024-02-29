b766993f93cdda3613e40c6560ea1f61fd8200d6
Athos Iury Alexandre de Moraes Gonçalves <88007943+AthosAlexandre@users.noreply.github.com>
2024-02-29 12:16:13 -0400

    colocando botao de navegacao lateral no modo celular

--- arquiteto-projeto/src/components/navegacao/Navegacao.vue
+++ arquiteto-projeto/src/components/navegacao/Navegacao.vue
@@ -13,6 +13,20 @@
                 <v-list-item title="CONTATO"></v-list-item>
             </v-list>
         </div>
+
+        
+            <v-app-bar-nav-icon class="bota-aparecer" @click="isAbrir = !isAbrir"></v-app-bar-nav-icon>
+        
+
+        <v-navigation-drawer v-model="isAbrir" location="right" >
+            <v-app-bar-nav-icon location="top center" class="bota-aparecer" @click="isAbrir = !isAbrir"></v-app-bar-nav-icon>
+            <v-list>
+                <v-list-item title="HOME"></v-list-item>
+                <v-list-item title="BLOG"></v-list-item>
+                <v-list-item title="CONTATO"></v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
     </v-container>
 </template>
 
@@ -21,7 +35,7 @@ export default {
     name: 'Navegacao',
     data() {
         return {
-
+            isAbrir: false
         }
     },
 }
@@ -31,5 +45,19 @@ export default {
 .bg {
     background: rgba(21, 21, 21, 0);
     color: white;
+}
+
+.bota-aparecer{
+    display: none;
+}
+
+@media screen and (max-width: 600px){
+    .bota-aparecer{
+        display: block;
+    }
+    .container-navegacao{
+        display: none;
+    }
+    
 }
 </style>
