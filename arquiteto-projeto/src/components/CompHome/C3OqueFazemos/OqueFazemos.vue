e15b77d8d127ac777db7bb0e77cacf59b7051f44
Athos Iury Alexandre de Moraes Gonçalves <88007943+AthosAlexandre@users.noreply.github.com>
2024-03-02 14:24:27 -0400

    colocando o container dos cards em cima do container da imagem

--- arquiteto-projeto/src/components/CompHome/C3OqueFazemos/OqueFazemos.vue
+++ arquiteto-projeto/src/components/CompHome/C3OqueFazemos/OqueFazemos.vue
@@ -35,6 +35,7 @@
                 </v-row>
             </v-container>
         </section>
+        
     </article>
 </template>
 
@@ -116,13 +117,15 @@ export default {
 }
 
 .cointainer-cards-oq-fazemos{
-    
+    position: relative;
+    z-index: 2; 
+    margin-top: -150px; 
 }
 
 .bordeBox{
-    border: 1px solid #e0e0e0;
     border-radius: 5px;
     box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+    background-color: #cacaca;
 }
 
 .dimencao-card{
