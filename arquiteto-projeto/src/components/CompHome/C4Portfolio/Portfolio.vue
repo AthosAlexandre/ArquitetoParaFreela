2abddcc73dff1bac2f61e5fc742b3d65427f3172
Athos Iury Alexandre de Moraes Gonçalves <88007943+AthosAlexandre@users.noreply.github.com>
2024-03-03 08:28:37 -0400

    criando o componente do Portfolio

---
+++ arquiteto-projeto/src/components/CompHome/C4Portfolio/Portfolio.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <v-container>
+            
+        </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Portfolio',
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
